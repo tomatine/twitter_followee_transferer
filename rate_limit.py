f54273c77932c2383e423f3e5e5107f1a157ec08
tomatine <tomatine86@gmail.com>
2020-09-28 02:54:43 +0900

    とりあえず完成？

--- rate_limit.py
+++ rate_limit.py
@@ -8,11 +8,10 @@ auth.set_access_token(AT,AS)
 
 api = tweepy.API(auth)
 
-#print(json.dumps(limit,indent=4))
 
 limit = api.rate_limit_status()
 
-#print(json.dumps(limit,indent=4))
+print(json.dumps(limit,indent=4))
 
 print("API.followers")
 print(limit["resources"]["followers"]["/followers/list"],"\n" +"-"*50)
@@ -33,4 +32,7 @@ print("API.search")
 print(limit["resources"]["search"]["/search/tweets"],"\n" +"-"*50)
 
 print("API.rate_limit_status")
-print(limit["resources"]["application"]["/application/rate_limit_status"],"\n" +"-"*50)
+print(limit["resources"]["application"]["/application/rate_limit_status"], "\n" + "-" * 50)
+
+print("API.friendships_outgoing")
+print(limit["resources"]["friendships"]["/friendships/outgoing"], "\n" + "-" * 50)
--- twitter_followee_transferer.py
+++ twitter_followee_transferer.py
@@ -1,17 +1,27 @@
 import tweepy
-from credential import CK,CS,AT,AS
+from credential import CK, CS, AT, AS
+from settings import pre_screen_name,post_screen_name
 
 auth = tweepy.OAuthHandler(CK,CS)
 auth.set_access_token(AT,AS)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-public_tweets = api.home_timeline()
+pre_follower = {id for id in tweepy.Cursor(api.followers_ids, screen_name=pre_screen_name).items()}
+pre_followee = {id for id in tweepy.Cursor(api.friends_ids, screen_name=pre_screen_name).items()}
+post_followee = {id for id in tweepy.Cursor(api.friends_ids, screen_name=post_screen_name).items()}
 
+print(pre_followee)
+print(len(pre_followee))
+print(pre_follower)
+print(len(pre_follower))
+print(post_followee)
+print(len(post_followee))
 
-followers_set = api.followers(skip_status=True, include_user_entities=False,count=200)
+target_for_transfer = pre_followee - pre_follower - post_followee
+print(target_for_transfer)
+print(len(target_for_transfer))
 
-for follower in followers_set:
-    print(follower.name)
-
-print(len(followers_set))
+for id in target_for_transfer:
+    api.create_friendship(id=id)
+    print(post_screen_name + ' followed ' + api.get_user(id=id).screen_name)
